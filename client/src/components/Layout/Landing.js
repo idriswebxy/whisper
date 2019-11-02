d9c1a91950e940ce9d639836e8c1573f86706df0
Idris <icoder112@gmail.com>
2019-11-02 18:25:32 -0400

    updates

--- client/src/components/Layout/Landing.js
+++ client/src/components/Layout/Landing.js
@@ -8,10 +8,10 @@ import Particles from "react-particles-js";
 const particlesOptions = {
   particles: {
     number: {
-      value: 50,
+      value: 60,
       density: {
         enable: true,
-        value_area: 800
+        value_area: 1000
       }
     }
   }
