a18f609c55dd241e7e9f0b4eb4b4df6c9d7f39c0
Idris <icoder112@gmail.com>
2019-11-02 20:34:55 -0400

    updates

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -91,7 +91,7 @@ export const login = (email, password) => async dispatch => {
   const body = JSON.stringify({ email, password });
 
   try {
-    const res = await axios.post("/api/auth", body, config, { timeout: 10 });
+    const res = await axios.post("/api/auth", body, config);
     
     dispatch({
       type: LOGIN_SUCCESS,
