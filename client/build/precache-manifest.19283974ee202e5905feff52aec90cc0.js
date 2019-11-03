c828c1b168b3e443f23292c0a9a2f332833e0da4
Idris <icoder112@gmail.com>
2019-11-02 21:41:58 -0400

    updates

---
+++ client/build/precache-manifest.19283974ee202e5905feff52aec90cc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f7669e20ff9c26ca64cf8553330243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09bd4ca91fdc9aa62c60",
+    "url": "/static/css/main.1a7d5e4c.chunk.css"
+  },
+  {
+    "revision": "b5eb3f9de2b3b8d48c00",
+    "url": "/static/js/2.cabf02d4.chunk.js"
+  },
+  {
+    "revision": "09bd4ca91fdc9aa62c60",
+    "url": "/static/js/main.b2a362ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
